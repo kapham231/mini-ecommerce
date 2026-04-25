d7f48f7b87390246434ef29e1d58d4c9b8a6e9a1
Pham Anh Kiet <kapham231@gmail.com>
2026-04-25 14:57:17 +0700

    add cart model

---
+++ backend/prisma/migrations/20260425074437_update_cart_order/migration.sql
@@ -0,0 +1,57 @@
+/*
+  Warnings:
+
+  - The values [CONFIRMED] on the enum `Order_status` will be removed. If these variants are still used in the database, this will fail.
+  - Added the required column `name` to the `OrderItem` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `order` ADD COLUMN `completedAt` DATETIME(3) NULL,
+    ADD COLUMN `paidAt` DATETIME(3) NULL,
+    ADD COLUMN `shippedAt` DATETIME(3) NULL,
+    MODIFY `status` ENUM('PENDING', 'PAID', 'SHIPPED', 'COMPLETED', 'CANCELLED') NOT NULL DEFAULT 'PENDING';
+
+-- AlterTable
+ALTER TABLE `orderitem` ADD COLUMN `imageUrl` VARCHAR(191) NULL,
+    ADD COLUMN `name` VARCHAR(191) NOT NULL;
+
+-- CreateTable
+CREATE TABLE `Cart` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Cart_userId_key`(`userId`),
+    INDEX `Cart_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CartItem` (
+    `id` VARCHAR(191) NOT NULL,
+    `cartId` VARCHAR(191) NOT NULL,
+    `productId` VARCHAR(191) NOT NULL,
+    `quantity` INTEGER NOT NULL,
+
+    INDEX `CartItem_cartId_idx`(`cartId`),
+    UNIQUE INDEX `CartItem_cartId_productId_key`(`cartId`, `productId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Cart` ADD CONSTRAINT `Cart_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CartItem` ADD CONSTRAINT `CartItem_cartId_fkey` FOREIGN KEY (`cartId`) REFERENCES `Cart`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CartItem` ADD CONSTRAINT `CartItem_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- RedefineIndex
+CREATE INDEX `Order_userId_idx` ON `Order`(`userId`);
+DROP INDEX IF EXISTS `Order_userId_fkey` ON `order`;
+
+-- RedefineIndex
+CREATE INDEX `OrderItem_orderId_idx` ON `OrderItem`(`orderId`);
+DROP INDEX IF EXISTS `OrderItem_orderId_fkey` ON `orderitem`;
